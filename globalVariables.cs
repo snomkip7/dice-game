8217331af8551977b030e15cba4567f23bf6102c
sheepapple <sheepapple00@gmail.com>
2024-04-18 19:32:31 -0400

    wip

---
+++ globalVariables.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+Vector2 mousePosition = new(0, 0);
+bool isMouseDragging = false;
+public partial class globalVariables : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
