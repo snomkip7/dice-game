2f0ea39bd7e5e52881bc2c7176e1bc8425a1cb5a
sheepapple <sheepapple00@gmail.com>
2024-04-28 21:14:47 -0400

    DiceBuilding updates (ALMOST WORKING)

--- DiceBuilding/DieFace.cs
+++ DiceBuilding/DieFace.cs
@@ -2,6 +2,7 @@ using Godot;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
 public partial class DieFace : Node2D
@@ -13,15 +14,20 @@ public partial class DieFace : Node2D
 	public String face = "";
 	private const float FollowSpeed = 2.0f;
 	private Vector2 returnPos;
+	private Vector2 startPos;
 	private static globalVariables globalVariables = new();
-	//private bool draggable = false;
-	private bool inSlot = false;
 	private Vector2 lastLerp = new();
+	private Godot.Collections.Array<Area2D> oLapAreas;
+	private Area2D area;
 	public override void _Ready()
 	{
+
 	//	this.Connect("LiftUpEventHandler", DieBG, "OnLiftUp");
 		globalVariables = GetNode<globalVariables>("/root/GlobalVariables");
 		returnPos = this.Position;
+		startPos = returnPos;
+		area = GetNode<Area2D>("Area2D");
+		oLapAreas = area.GetOverlappingAreas();
 		GetNode<Sprite2D>("DieBG").GetNode<AnimatedSprite2D>("Faces").Frame = Face2int(face);
 	}
 	public static int Face2int(string newFace){
@@ -35,15 +41,26 @@ public partial class DieFace : Node2D
             _ => 0,
         };
     }
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		//REMOVE THIS LATER
+		switch(area.IsInGroup("slotted")){
+			case(true):
+				GetNode<Sprite2D>("DieBG").Modulate = new Color(0, 0, 0);
+				break;
+			case(false):
+				GetNode<Sprite2D>("DieBG").Modulate = new Color(255,255,255);
+				break;
+		}
+
+
 		if(Input.IsActionJustPressed("leftClick")){
+			if(lastLerp == new Vector2(0,0)){lastLerp = globalVariables.lastMousePos;}
+			area.SetCollisionMaskValue(2, true); // Disables detection with other slots on the way back
+
 			if(globalVariables.mousePosition.DistanceTo(Position) < 125){
-				GD.Print(face, "");
 				active = true;
-				returnPos = this.Position;
+				returnPos = this.Position;	
 				this.EmitSignal("liftUp", active);
 			}
 		} else if(!Input.IsActionPressed("leftClick")){
@@ -52,32 +69,33 @@ public partial class DieFace : Node2D
 			}
 		}
 
-		if(lastLerp == new Vector2(0,0)){
-			lastLerp = globalVariables.lastMousePos;
+		if(area.HasOverlappingAreas()){
+			var oLap = area.GetOverlappingAreas();
+			Vector2 closestArea = oLap[0].Position;
+			for(int i = 1; i < oLap.Count; i++){
+				if(oLap[i].IsInGroup("Slot")){
+					if(Position.DistanceTo(oLap[i].Position) < Position.DistanceTo(closestArea)){
+						closestArea = oLap[i].Position;
+					}
+				}
+			}
+			returnPos = closestArea;
+			//GD.Print(closestArea, "");
+		} else {
+			//GD.Print("else statement");
+			//returnPos = startPos;
 		}
-		//GD.Print(active, "");
-		
+
 		if(active){
-			//GD.Print(face, "");
-			//this.Position = globalVariables.mousePosition;
 			this.Position = lastLerp;
 			lastLerp = globalVariables.lastMousePos.Lerp(globalVariables.mousePosition, (float)delta * FollowSpeed);
 		} else {
+			area.SetCollisionMaskValue(2, false);
 			if(this.Position != returnPos){
 				var tween = GetTree().CreateTween();
 				tween.TweenProperty(this, "position", returnPos, 0.2);
 			}
 			this.EmitSignal("liftUp", active);
-		}
-	}
-	
-	public static void OnMouseButtonPressed(){
-		return;
-	}
-
-	private void OnAreaEntered(Area2D body){
-		if(active){
-			returnPos = body.Position;
 		}
 	}
 }
--- DiceBuilding/dbManager.cs
+++ DiceBuilding/dbManager.cs
@@ -1,16 +1,35 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class dbManager : Control
 {
-	
+	//public static List<DieSlot> DieSlots = new();
 	// Called when the node enters the scene tree for the first time.
+	private static globalVariables globalVariables = new();
 	public override void _Ready()
 	{
+		globalVariables = GetNode<globalVariables>("/root/GlobalVariables");
+		return;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		return;
+	}
+
+	public static bool MatchMaker(Area2D face, Area2D slot){
+		return false;
+	}
+
+	public void SaveDice(){
+		String[] savedFaces = new String[7];
+		for(int i = 0; i < 6; i++){
+			DieFace curDie = GetNode<Node2D>("DieFace" + i) as DieFace;
+			savedFaces[i] = curDie.face;
+		}
+		globalVariables.dieEffects = savedFaces;
+		GetTree().ChangeSceneToFile("res://MainMenu.tscn");
 	}
 }
